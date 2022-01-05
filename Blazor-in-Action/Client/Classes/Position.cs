127fb78a56e2e1d733983a7b9dfeb880b91cdbc4
najielkotob <naji@dotnetheroes.com>
2022-01-05 15:32:14 +0200

    Open Weather

---
+++ Blazor-in-Action/Client/Classes/Position.cs
@@ -0,0 +1,8 @@
+﻿namespace Blazor_in_Action.Client.Classes;
+
+public class Position
+{
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+}
+
---
+++ Blazor-in-Action/Client/wwwroot/scripts/openWeatherInterop.js
@@ -0,0 +1,18 @@
+﻿var openWeatherInterop = {};
+openWeatherInterop.getPosition = async function () {
+    function getPositionAsync() {
+        return new Promise((success, error) => {
+            navigator.geolocation.getCurrentPosition(success, error);
+        });
+    }
+    if (navigator.geolocation) {
+        var position = await getPositionAsync();
+        var coords = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        };
+        return coords;
+    } else {
+        throw Error("Geolocation is not supported by this browser.");
+    };
+}
